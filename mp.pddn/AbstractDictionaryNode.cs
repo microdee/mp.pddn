32935f612106c1f34b2c00dd815e9e5e919c2d92
David Morasz <morasz@meso.net>
2018-12-21 23:30:09 +0100

    now AbstractDictionaryNode is even more generic and it can handle any type inheriting from IDictionary with the desired key value types

--- mp.pddn/AbstractDictionaryNode.cs
+++ mp.pddn/AbstractDictionaryNode.cs
@@ -9,11 +9,48 @@ using VVVV.PluginInterfaces.V2;
 
 namespace mp.pddn
 {
+    /// <summary>
+    /// Static class for following object changes
+    /// </summary>
+    public static class ObjectChange
+    {
+        /// <summary>
+        /// Object change counters based on hashcode of object
+        /// </summary>
+        private static Dictionary<int, int> ChangeCounters { get; } = new Dictionary<int, int>();
+
+        public static int NotifyChange(this object obj)
+        {
+            var hc = obj.GetHashCode();
+            if (ChangeCounters.ContainsKey(hc))
+                ChangeCounters[hc]++;
+            else ChangeCounters.Add(hc, 1);
+            return ChangeCounters[hc];
+        }
+
+        public static bool CheckChanged(this object obj, ref int refchangecount)
+        {
+            var hc = obj.GetHashCode();
+            if (ChangeCounters.ContainsKey(hc))
+            {
+                var res = ChangeCounters[hc] != refchangecount;
+                refchangecount = ChangeCounters[hc];
+                return res;
+            }
+            else
+            {
+                refchangecount = -1;
+                return false;
+            }
+        }
+    }
+
     public class AbstractDictionaryNode<TKey, TVal> : IPluginEvaluate, IPartImportsSatisfiedNotification
     {
         #region fields & pins
+
         [Input("Dictionary In")]
-        public Pin<Dictionary<TKey, TVal>> FDictIn;
+        public Pin<IDictionary<TKey, TVal>> FDictIn;
 
         [Input("Default Keys")]
         public IDiffSpread<TKey> FKeys;
@@ -39,13 +76,21 @@ namespace mp.pddn
         public Pin<TKey> FGetKey;
 
         [Output("Dictionary Out")]
-        public ISpread<Dictionary<TKey, TVal>> FDictOut;
+        public ISpread<IDictionary<TKey, TVal>> FDictOut;
         [Output("Keys")]
         public ISpread<TKey> FKeysOut;
         [Output("Values")]
         public ISpread<ISpread<TVal>> FOut;
 
-        private Dictionary<TKey, TVal> dict = new Dictionary<TKey, TVal>();
+        [Output(
+            "Changed",
+            IsBang = true,
+            Visibility = PinVisibility.Hidden
+        )]
+        public ISpread<bool> FChanged;
+
+        private IDictionary<TKey, TVal> _dict = new Dictionary<TKey, TVal>();
+        private int _dictChangeCounter = -1;
 
         [Import()]
         public ILogger FLogger;
@@ -53,7 +98,7 @@ namespace mp.pddn
 
         public void OnImportsSatisfied()
         {
-            FDictIn.Disconnected += (sender, args) => dict = new Dictionary<TKey, TVal>();
+            FDictIn.Disconnected += (sender, args) => _dict = new Dictionary<TKey, TVal>();
         }
 
         //called when data for any output pin is requested
@@ -61,20 +106,25 @@ namespace mp.pddn
         {
             if (FDictIn.IsConnected)
             {
-                dict = FDictIn[0];
+                _dict = FDictIn[0];
             }
-            if(FClear[0]) dict.Clear();
+            if(FClear[0])
+            {
+                _dict.Clear();
+                _dict.NotifyChange();
+            }
             if (FResetDefault[0])
             {
-                dict.Clear();
+                _dict.Clear();
                 if (FDef.SliceCount != 0)
                 {
                     for (int i = 0; i < FKeys.SliceCount; i++)
                     {
-                        if (!dict.ContainsKey(FKeys[i]))
-                            dict.Add(FKeys[i], FDef[i]);
+                        if (!_dict.ContainsKey(FKeys[i]))
+                            _dict.Add(FKeys[i], FDef[i]);
                     }
                 }
+                _dict.NotifyChange();
             }
             if (FSet.IsChanged)
             {
@@ -84,9 +134,10 @@ namespace mp.pddn
                     {
                         if(!FSet[i]) continue;
 
-                        if (dict.ContainsKey(FMod[i]))
-                            dict[FMod[i]] = FVal[i];
-                        else dict.Add(FMod[i], FVal[i]);
+                        if (_dict.ContainsKey(FMod[i]))
+                            _dict[FMod[i]] = FVal[i];
+                        else _dict.Add(FMod[i], FVal[i]);
+                        _dict.NotifyChange();
                     }
                 }
             }
@@ -98,9 +149,10 @@ namespace mp.pddn
                     {
                         if (!FRemoveAdd[i]) continue;
 
-                        if (dict.ContainsKey(FMod[i]))
-                            dict.Remove(FMod[i]);
-                        else dict.Add(FMod[i], FVal[i]);
+                        if (_dict.ContainsKey(FMod[i]))
+                            _dict.Remove(FMod[i]);
+                        else _dict.Add(FMod[i], FVal[i]);
+                        _dict.NotifyChange();
                     }
                 }
             }
@@ -112,48 +164,66 @@ namespace mp.pddn
                     for (int i = 0; i < FRemoveKey.SliceCount; i++)
                     {
                         if (!FRemove[i]) continue;
-                        if (dict.ContainsKey(FRemoveKey[i])) dict.Remove(FRemoveKey[i]);
+                        if (_dict.ContainsKey(FRemoveKey[i])) _dict.Remove(FRemoveKey[i]);
+                        _dict.NotifyChange();
                     }
                 }
             }
 
+            if (FDictIn.IsChanged && FDictIn.SliceCount > 0)
+                _dict.NotifyChange();
+
+            var changed = _dict.CheckChanged(ref _dictChangeCounter);
+
             if (FGetKey.IsConnected)
             {
-                FOut.SliceCount = FKeysOut.SliceCount = FGetKey.SliceCount;
-                for (int i = 0; i < FGetKey.SliceCount; i++)
+                if(changed || FGetKey.IsChanged)
                 {
-                    if (FGetKey[i] == null)
+                    FOut.SliceCount = FKeysOut.SliceCount = FGetKey.SliceCount;
+                    for (int i = 0; i < FGetKey.SliceCount; i++)
                     {
-                        FOut[i].SliceCount = 0;
-                        continue;
-                    }
+                        if (FGetKey[i] == null)
+                        {
+                            FOut[i].SliceCount = 0;
+                            continue;
+                        }
 
-                    if (!dict.TryGetValue(FGetKey[i], out var res))
-                    {
-                        FOut[i].SliceCount = 0;
-                        continue;
-                    }
+                        if (!_dict.TryGetValue(FGetKey[i], out var res))
+                        {
+                            FOut[i].SliceCount = 0;
+                            continue;
+                        }
 
-                    FKeysOut[i] = FGetKey[i];
-                    FOut[i].SliceCount = 1;
-                    FOut[i][0] = res;
+                        FKeysOut[i] = FGetKey[i];
+                        FOut[i].SliceCount = 1;
+                        FOut[i][0] = res;
+                    }
                 }
             }
             else
             {
-                FOut.SliceCount = dict.Count;
-                FKeysOut.SliceCount = dict.Count;
-                int ii = 0;
-                foreach (var kvp in dict)
+                if(changed)
                 {
-                    FOut[ii].SliceCount = 1;
-                    FOut[ii][0] = kvp.Value;
-                    FKeysOut[ii] = kvp.Key;
-                    ii++;
+                    FOut.SliceCount = _dict.Count;
+                    FKeysOut.SliceCount = _dict.Count;
+                    int ii = 0;
+                    foreach (var kvp in _dict)
+                    {
+                        FOut[ii].SliceCount = 1;
+                        FOut[ii][0] = kvp.Value;
+                        FKeysOut[ii] = kvp.Key;
+                        ii++;
+                    }
                 }
             }
 
-            FDictOut[0] = dict;
+            FChanged[0] = changed;
+            FDictOut.Stream.IsChanged = false;
+            if (FDictOut[0]?.GetHashCode() != _dict?.GetHashCode())
+            {
+                FDictOut.Stream.IsChanged = true;
+                FDictOut[0] = _dict;
+            }
         }
 
     }
